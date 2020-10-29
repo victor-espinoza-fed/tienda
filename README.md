4985e3493ef8d92cbaeacd75aa67be5ee4194e7e
victor-espinoza-fed <victor.espinoza.fed@gmail.com>
2020-10-29 17:44:02 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-w5ajt6
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-w5ajt6)
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+import "./style.scss";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+      <Button>Hello</Button>
+    </div>
+  );
+}
